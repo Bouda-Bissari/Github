a596a288fa037c7bff967fd0e01f326b9ed65621
Bouda-Bissari <jeanmariebissari@gmail.com>
2024-01-12 07:51:44 +0000

    premiere version

---
+++ bootstrap-5.3.2-dist/Script.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    // Sélectionner la barre de navigation Bootstrap
+    var navbar = document.querySelectorAll('p');
+
+    // Ajouter une classe personnalisée pour changer le style
+    navbar.style.backgroundColor = '#e74c3c'; 
+}
